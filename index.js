a6d903084fe5f894a6abfcd6c1b92c9c3cefcbc7
fun to dev <51599465+Ziro01@users.noreply.github.com>
2022-02-03 16:44:36 +0700

    updete

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-// var io = require('socket.io')({
-// 	transports: ['websocket'],
-// });
-
-// io.attach(4567);
-
-// io.on('connection', function(socket){
-// 	socket.on('beep', function(){
-// 		socket.emit('boop');
-// 	});
-// })
-
-//**************************** */
-// const WebSocket = require('ws')
-// const wss = new WebSocket.Server({ port: 6321 },()=>{
-//     console.log('server started')
-// })
-// wss.on('connection', function connection(ws) {
-//    ws.on('message', (data) => {
-//       console.log('data received \n %o',data)
-//       ws.send(data);
-//    })
-// })
-// wss.on('listening',()=>{
-//    console.log('listening on 8080')
-// })
-//******************************************* */
-
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 },()=>{
     console.log('server started')
